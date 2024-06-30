813e5d1b2d727cf2cb2384dcc22273402d82afa7
AHRkUyProgram <hugaalbin@gmail.com>
2024-07-01 04:49:14 +0700

    first commit

---
+++ app/Views/footer.php
@@ -0,0 +1,12 @@
+</div>
+</div>
+<script type="text/javascript">
+       var marker = L.marker([-7.666241098580379, 109.05278603606298]).bindPopup("<h3>Abhirama Huga Ramadhan</h3><p>212221009<br>Informatika<hr>@aku_huga02</p><img src='public/uploads/huga.png'style='width:100px;'>").openPopup().addTo(map);
+
+$( document ).ready(function() {
+// untuk menghilangkan pesan data berhasil disimpan,dll
+$('.alert').delay(2000).fadeOut('slow');
+});
+</script>
+</body>
+</html>
